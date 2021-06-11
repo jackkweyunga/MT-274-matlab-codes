dadf4fa62f4848dc5669a061599d632dddff4432
Brightius Kalokola <69508559+Kalokola@users.noreply.github.com>
2021-06-11 22:33:15 +0300

    Delete fundertask1.m

--- fundertask1.m
+++
@@ -1,5 +0,0 @@
-% my codes look long & wordy, but they show the actual roadmap
-% my aim is to make every one understand the real thing
-function derval = fundertask1(x)
-  derval = cosh(x)+sin(x);
-endfunction
