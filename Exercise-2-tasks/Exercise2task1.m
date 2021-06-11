fc4620b8a11147482d43f14804172e493a0be60f
Brightius Kalokola <69508559+Kalokola@users.noreply.github.com>
2021-06-11 23:42:44 +0300

    Update Exercise2task1.m

--- Exercise-2-tasks/Exercise2task1.m
+++ Exercise-2-tasks/Exercise2task1.m
@@ -8,7 +8,7 @@ a = 1; b = 2;
 % using IVT to test existence of roots in the interval [a,b]
 outs = funtask1(a)*funtask1(b);
 if outs < 0
-  disp(["Function f(x) has a root between  ",num2str(a)," and  ",num2str(b)])
+  fprintf("Function f(x) has a root between  %i and  %i\n",a,b)
   counter = 1; precdps = 6; root = [];
   while true
       p = (1/2)*(a + b);
@@ -25,9 +25,8 @@ if outs < 0
       end  
       counter ++;
   end
-  disp(["Precision: (",num2str(precdps),"dps)"]) 
-  disp(["Iterations: ",num2str(counter),'  ',"Root: ",num2str(root(counter))])
-  disp(["f(",num2str(root(counter)),") = ",num2str(funtask1(root(counter)))," approximately = 0"])
+  fprintf("Precision: %i (dps) Iterations: %i   Root: %i\n",precdps,counter,root(counter))
+  fprintf("f(%i) = %i approximately = 0\n",root(counter),funtask1(root(counter)))
 else
-  disp("Root doesn't exist.! Retry another interval")
+  fprintf("Root doesn't exist.! Retry another interval")
 end
